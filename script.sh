5277205fce68c754fc604b11d6f6dc24627954d8
BrennaRamos <bramo033@fiu.edu>
2023-02-07 20:03:33 -0400

    testing

--- script.sh
+++ script.sh
@@ -7,29 +7,32 @@ random_number=$((1 + $RANDOM % 50))
 # Initialize number of tries to zero
 
 # Loop 5 times to give the user chances to guess the number
-  for tries in (tries = 0; tries < 5; tries++)
+  for ((tries=1; tries<=5; tries++))
     do 
   # Read user input as guess
-      remaining = 5 - tries
+      remaining=$((5-tries))
       echo "Input your guess" 
-      read guess
+      read -p 'Guess: ' guess
+      guess=$guess
   # Check if guess is correct
       
-      if guess > random_number
+      if ((guess>random_number))
         then
         echo "Your guess was too high, you have $remaining tries left"
       fi
-      if guess = random_number
+      if ((guess==random_number))
         then 
         echo "You got it! You used $tries tries"
         break
-      if guess < random_number
+      fi
+      if ((guess<random_number))
         then
         echo "Your guess was too low, you have $remaining tries left"
       fi
-      if tries = 4
+      if ((tries==5))
         then
         echo "Game Over. Ran out of tries."
+      fi
   done
    
   
